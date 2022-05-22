49254dd454138eb1dbe9cd7b360fc3ec049341c7
zvdy <78762257+zvdy@users.noreply.github.com>
2022-05-22 12:25:46 -0500

    Remove map objects

--- maze.py
+++ maze.py
@@ -20,11 +20,18 @@ while True:
         for coordinate_x in range(MAP_WIDTH):
 
             char_to_draw = " "
+            object_in_cell = None
+
             for map_object in map_objects:
                 if map_object[POS_X] == coordinate_x and map_object[POS_Y] == coordinate_y:
                     char_to_draw =  "*"
+                    object_in_cell = map_object
+
             if my_position[POS_X] == coordinate_x and my_position[POS_Y] == coordinate_y:
                 char_to_draw =  "@"
+                if object_in_cell:
+                    map_objects.remove(object_in_cell)
+
             print(" {} ".format(char_to_draw), end="")
         print("|")
 
