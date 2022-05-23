49b8fa0c89464800d0f196ce4bb462330f9fcc3e
zvdy <78762257+zvdy@users.noreply.github.com>
2022-05-23 13:48:17 -0500

    map object automatic generation & dying feature

--- snake.py
+++ snake.py
@@ -11,17 +11,19 @@ my_position = [6, 3]
 tail_length = 0
 tail = []
 map_objects = []
-
-# Generate random objects on the map
-while len(map_objects) < NUM_OF_MAP_OBJECTS:
-    new_position = [random.randint(0,MAP_WIDTH), random.randint(0, MAP_HEIGHT)]
-
-    if new_position not in map_objects and new_position != my_position:
-        map_objects.append(new_position)
+end_game = False
+died = False
 
 # Main Loop
-while True:
-    # draw map
+while not end_game:
+    # Generate random objects on the map
+    while len(map_objects) < NUM_OF_MAP_OBJECTS:
+        new_position = [random.randint(0,MAP_WIDTH), random.randint(0, MAP_HEIGHT)]
+
+        if new_position not in map_objects and new_position != my_position:
+            map_objects.append(new_position)
+    
+    # Draw map
     print("+" + "-" * MAP_WIDTH * 3 + "+")
 
     for coordinate_y in range(MAP_HEIGHT):
@@ -31,6 +33,7 @@ while True:
 
             char_to_draw = " "
             object_in_cell = None
+            tail_in_cell = None
 
             for map_object in map_objects:
                 if map_object[POS_X] == coordinate_x and map_object[POS_Y] == coordinate_y:
@@ -40,6 +43,7 @@ while True:
             for tail_piece in tail:
                 if tail_piece[POS_X] == coordinate_x and tail_piece[POS_Y] == coordinate_y:
                     char_to_draw = "@"
+                    tail_in_cell = tail_piece
          
             if my_position[POS_X] == coordinate_x and my_position[POS_Y] == coordinate_y:
                 char_to_draw =  "@"
@@ -47,6 +51,10 @@ while True:
                 if object_in_cell:
                     map_objects.remove(object_in_cell)
                     tail_length += 1
+
+                if tail_in_cell:
+                    end_game = True
+                    died = True
 
             print(" {} ".format(char_to_draw), end="")
         print("|")
@@ -79,5 +87,8 @@ while True:
         my_position[POS_X] += 1
         my_position[POS_X] %= MAP_WIDTH
     elif direction == "q":
-       break
+       end_game = True
     os.system("clear")
+
+if died:
+    print("You died!")
